6520e096407bd9bf40de589ff49114f5d52432df
Xhway <dhway20@gmail.com>
2023-04-30 22:55:23 -0400

    upadated webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,19 +11,59 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest:'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name:'JATE',
+        start_url: '/',
+        publicPath:'/',
+        icons:[{
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },],
+      })
       
     ],
 
     module: {
       rules: [
-        
+        {
+          test:/\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options:{
+              presets:['@babel/preset-env'],
+              plugins:['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
